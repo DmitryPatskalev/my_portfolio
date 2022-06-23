231b5e24df01ad1e4a5805cd6b5888a0d550b964
DmitryPatskalev <dimanord@yandex.ru>
2022-06-23 16:34:20 +0300

    commit

---
+++ src/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Main.module.css'
+
+const Main = () => {
+  return (
+    <div className={style.mainBlock}>
+      <div className={style.text}></div>
+      <div className={style.photo}></div>
+
+    </div>
+  );
+};
+
+export default Main;
