4a44e8519afefa806d4d3f317e48efccd463c0f9
DmitryPatskalev <dimanord@yandex.ru>
2022-06-23 18:01:54 +0300

    commit

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import style from './Main.module.css'
+import styleContainer from './../Common/Styles/Container.module.css'
 
 const Main = () => {
   return (
     <div className={style.mainBlock}>
-      <div className={style.text}></div>
-      <div className={style.photo}></div>
-
+      <div className={styleContainer.container}>
+        <div className={style.text}>
+          <span>Hi everyone!!!</span>
+          <h1>I am Dmitry Patskalev</h1>
+            <p>Front-end developer<br/>
+              HTML/CSS/JS/REACT/TS</p>
+        </div>
+        <div className={style.photo}></div>
+      </div>
     </div>
   );
 };
