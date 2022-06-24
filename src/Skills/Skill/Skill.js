b20aa719d3494c32f43b62e154e94688eba6f72c
DmitryPatskalev <dimanord@yandex.ru>
2022-06-24 15:37:01 +0300

    commit

--- src/Skills/Skill/Skill.js
+++ src/Skills/Skill/Skill.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './Skill.module.css'
 
+
 const Skill = (props) => {
   return (
     <div className={style.skill}>
-      <div className={style.icon}></div>
+      <div className={style.icon}>{props.logo}</div>
         <h3>{props.title}</h3>
         <span className={style.description}>{props.description}</span>
     </div>
--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './Skills.module.css'
 import styleContainer from './../Common/Styles/Container.module.css'
 import Skill from "./Skill/Skill";
+import html5 from './../Photo/html5.jpg'
 
 const Skills = () => {
   return (
@@ -9,13 +10,15 @@ const Skills = () => {
       <div className={`${styleContainer.container} ${style.skillsContainer}`}>
         <h2 className={style.title}>My Skills</h2>
         <div className={style.skills}>
-          <Skill title='HTML/CSS/JS' description='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'/>
+          <Skill title='HTML/CSS' logo={<img src={html5} className={style.html} alt='logo' />} description='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'/>
 
-          <Skill title='REACT'
+          <Skill title='JS'
                  description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur delectus deserunt maiores optio quia!'/>
 
-          <Skill title='TS' description=' Consequatur delectus deserunt maiores optio quia!'/>
-
+          <Skill title='REACT' description=' Consequatur delectus deserunt maiores optio quia!'/>
+          <Skill title='TypeScript' description='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'/>
+          <Skill title='NodeJS' description='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'/>
+          <Skill title='Basic Python' description='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'/>
         </div>
       </div>
     </div>
