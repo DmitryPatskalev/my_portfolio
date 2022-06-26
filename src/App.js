8ec1dd7d945be5dc2403b40c4a76b2588831e2f7
DmitryPatskalev <dimanord@yandex.ru>
2022-06-26 18:15:12 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main/Main";
 import Skills from "./Skills/Skills";
 import Projects from "./Projects/Projects";
 import WhereToWork from "./WhereToWork/WhereToWork";
+import Contact from "./Contact/Contact";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Skills/>
       <Projects/>
       <WhereToWork/>
+      <Contact/>
     </div>
   );
 }
