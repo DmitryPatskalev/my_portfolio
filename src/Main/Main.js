d7eeae5e2b0b4f04a54063cddbd2364f56e67012
DmitryPatskalev <dimanord@yandex.ru>
2022-06-27 22:49:41 +0300

    commit

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -7,13 +7,13 @@ const Main = () => {
   return (
     <div className={style.mainBlock}>
       <div className={styleContainer.container}>
-        <div className={style.text}>
-          <span>Hi everyone!!!</span>
-          <h1>I am Dmitry Patskalev</h1>
+          <div className={style.text}>
+            <span>Hi everyone!!!</span>
+            <h1>I am Dmitry Patskalev</h1>
             <p>Front-end developer<br/>
               HTML/CSS/JS/REACT/TS</p>
-        </div>
-        <div className={style.photo}>{<img src={loki} alt='loki' className={style.ava}/>}</div>
+          </div>
+          <div className={style.photo}>{<img src={loki} alt='loki' className={style.ava}/>}</div>
       </div>
     </div>
   );
